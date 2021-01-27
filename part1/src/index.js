61e7165694321174b2249c2a1fed37fded99537b
joaolopes <byjoao.lopes@bycom.pt>
2021-01-27 07:04:13 +0000

    Exercises 1.1 && 1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-      <div>Hello there</div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component'];
+  const exercises = [10, 7, 14];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
