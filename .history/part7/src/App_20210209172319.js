3d6964da34b59344ba2f6fa593392d0474c2e5c5
joaolopes <byjoao.lopes@bycom.pt>
2021-02-09 21:13:43 +0000

    Exercises 7.4 - 7.8

--- .history/part7/src/App_20210209172319.js
+++ .history/part7/src/App_20210209172319.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useHistory } from 'react-router-dom'
+import { useField } from './hooks/index'
 
 const Menu = () => {
 	const padding = {
@@ -73,18 +74,25 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-	const [content, setContent] = useState('')
-	const [author, setAuthor] = useState('')
-	const [info, setInfo] = useState('')
+	const content = useField('text')
+	const author = useField('text')
+	const info = useField('text')
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
+		console.log(content)
 		props.addNew({
-			content,
-			author,
-			info,
+			content: content.value,
+			autho: author.value,
+			info: info.value,
 			votes: 0,
 		})
+	}
+
+	const resetForm = () => {
+		content.reset()
+		author.reset()
+		info.reset()
 	}
 
 	return (
@@ -93,17 +101,20 @@ const CreateNew = (props) => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+					<input {...content} />
 				</div>
 				<div>
 					author
-					<input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+					<input {...author} />
 				</div>
 				<div>
 					url for more info
-					<input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+					<input {...info} />
 				</div>
 				<button>create</button>
+				<button type='button' onClick={resetForm}>
+					reset
+				</button>
 			</form>
 		</div>
 	)
@@ -131,7 +142,6 @@ const App = () => {
 	const [notification, setNotification] = useState('')
 
 	const addNew = (anecdote) => {
-		console.log('call')
 		anecdote.id = (Math.random() * 10000).toFixed(0)
 		setAnecdotes(anecdotes.concat(anecdote))
 		history.push('/')
